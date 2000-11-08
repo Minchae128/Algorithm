971367aaaa9f0f2d5a7630f3c985d63074810c64
shin <minchae.shin1@gmail.com>
2000-11-09 00:00:23 +0900

    23.11.08 Algorithm(수정)

--- src/Programmers/코딩테스트입문/Day12/Day12_3.java
+++ src/Programmers/코딩테스트입문/Day12/Day12_3.java
@@ -4,13 +4,20 @@ import java.util.Arrays;
 
 public class Day12_3 {
     public int solution(String my_string) {
+        // 영문 알파벳을 모두 제거
         my_string = my_string.replaceAll("[a-zA-Z]", "");
+        // 각 숫자를 문자열 배열로 분리
         String[] arr = my_string.split("");
 
+        // 합을 저장할 변수 초기화
         int answer = 0;
+
+        // 배열의 각 요소를 정수로 변환하여 합 계산
         for (int i = 0; i < arr.length; i++) {
             answer += Integer.valueOf(arr[i]);
         }
+
+        // 최종 합 반환
         return answer;
     }
 }
