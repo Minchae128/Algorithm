e58457eaacb02b6aeb35bec6d0de4dc041614387
smc <minchae.shin1@gmail.com>
2023-05-22 23:28:02 +0900

    Baejoon 2743

---
+++ Algorithm/Algorithm_2743.java
@@ -0,0 +1,31 @@
+package Algorithm;
+
+import java.util.Scanner;
+
+public class Algorithm_2743 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String a = in.nextLine();
+
+        System.out.println(a.length());
+
+    }
+}
+/*
+*
+* package Algorithm;
+
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+public class Algorithm_2743 {
+    private static final Logger LOGGER = Logger.getLogger(Algorithm_2743.class.getName());
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String a = in.nextLine();
+
+        LOGGER.info("Length of the input string: " + a.length());
+    }
+}
+*/
