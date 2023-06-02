792251dbe2d00de2159a7054eaf4f82f0a0c6c2c
Minchae128 <minchae.shin1@gmail.com>
2023-06-02 09:21:01 +0900

    23.06.02 Algorithm

---
+++ src/Baekjoon/Baekjoon_10718.java
@@ -0,0 +1,10 @@
+package src.Baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon_10718 {
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
