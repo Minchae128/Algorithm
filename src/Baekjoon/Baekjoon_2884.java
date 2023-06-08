ce7bcd464be90c7c9c639ebcaa6090bb4998d29e
smc <minchae.shin1@gmail.com>
2023-06-08 09:56:04 +0900

    23.06.08 Algorithm

---
+++ src/Baekjoon/Baekjoon_2884.java
@@ -0,0 +1,23 @@
+package src.Baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon_2884 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt(); // 시
+        int m = sc.nextInt(); // 분
+
+
+        if (m < 45) {
+            h--;
+            m = 60 - (45 - m);
+            if (h < 0) {
+                h = 23;
+            }
+            System.out.println(h + " " + m);
+        } else {
+            System.out.println(h + " " + (m - 45));
+        }
+    }
+}
