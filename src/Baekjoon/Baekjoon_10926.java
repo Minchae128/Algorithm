51ce67ac21fb5df4c1af70883edf86a0ac315368
smc <minchae.shin1@gmail.com>
2023-06-11 15:26:08 +0900

    23.06.11 Algorithm

--- src/Baekjoon/Baekjoon_10926.java
+++ src/Baekjoon/Baekjoon_10926.java
@@ -2,7 +2,7 @@ package src.Baekjoon;
 
 import java.util.Scanner;
 
-public class Baekjoon_8958 {
+public class Baekjoon_10926 {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     String n = sc.next();
