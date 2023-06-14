a68243df29de146a4665820dd54446b97b83d5f5
smc <minchae.shin1@gmail.com>
2023-06-14 09:40:22 +0900

    23.06.14 Algorithm

---
+++ src/Baekjoon/새싹/입력과계산/Baekjoon_1008.java
@@ -0,0 +1,15 @@
+package src.Baekjoon.새싹.입력과계산;
+
+import java.util.Scanner;
+
+public class Baekjoon_1008 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        double A = sc.nextInt();
+        double B = sc.nextInt();
+
+        System.out.print(A/B);
+
+    }
+}
---
+++ src/Baekjoon/새싹/입력과계산/Baekjoon_11382.java
@@ -0,0 +1,15 @@
+package src.Baekjoon.새싹.입력과계산;
+
+import java.util.Scanner;
+
+public class Baekjoon_11382 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+
+        System.out.println(a+b+c);
+    }
+}
