600aaeba168a99970f5e323c0a2747146b89626e
smc <minchae.shin1@gmail.com>
2023-06-14 10:22:35 +0900

    23.06.14 Algorithm

---
+++ src/Baekjoon/새싹/문자열/Baekjoon_11654.java
@@ -0,0 +1,14 @@
+package src.Baekjoon.새싹.문자열;
+
+import java.util.Scanner;
+
+public class Baekjoon_11654 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        char a = in.next().charAt(0);
+
+        System.out.println((int)a);
+
+    }
+
+}
