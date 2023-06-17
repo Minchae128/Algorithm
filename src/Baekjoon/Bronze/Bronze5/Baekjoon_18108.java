ab345b0d1f1038b8943a94f58da46032a8aa1dee
smc <minchae.shin1@gmail.com>
2023-06-17 11:07:54 +0900

    23.06.17 Algorithm

---
+++ src/Baekjoon/Bronze/Bronze5/Baekjoon_18108.java
@@ -0,0 +1,12 @@
+package src.Baekjoon.Bronze.Bronze5;
+
+import java.util.Scanner;
+
+public class Baekjoon_18108 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    System.out.println((n-543));
+    }
+}
