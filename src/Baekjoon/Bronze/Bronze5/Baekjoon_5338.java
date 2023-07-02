a45383d6535c8557462209d17572d515ffba392b
smc <minchae.shin1@gmail.com>
2023-07-02 17:12:32 +0900

    23.07.02 Algorithm

---
+++ src/Baekjoon/Bronze/Bronze5/Baekjoon_5338.java
@@ -0,0 +1,11 @@
+package src.Baekjoon.Bronze.Bronze5;
+
+public class Baekjoon_5338 {
+    public static void main(String[] args) {
+        System.out.println("       _.-;;-._");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-;;-._|");
+        System.out.println("'-..-'|   ||   |");
+        System.out.print("'-..-'|_.-''-._|");
+    }
+}
