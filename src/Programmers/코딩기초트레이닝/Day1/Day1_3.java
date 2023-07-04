9bc29c3295db4b65e4b500bb57530432f2cc56cf
smc <minchae.shin1@gmail.com>
2023-07-04 13:13:24 +0900

    23.07.04 Algorithm

--- src/Programmers/코딩기초트레이닝/Day1/Day1_3.java
+++ src/Programmers/코딩기초트레이닝/Day1/Day1_3.java
@@ -6,11 +6,18 @@ import java.util.Scanner;
 public class Day1_3 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        int n = sc.nextInt();
+        String str1 = sc.next();
+        String str2 = "";
 
-        for(int i = 0; i<n; i++){
-            System.out.print(str);
-        }
+        //아스키 코드표를 기준으로 대문자의 값과 소문자의 값이 32가 차이가 나는 점을 이용한다
+        for(char c : str1.toCharArray()){
+            if(c>=65&&c<=90){ //대문자를 소문자로 변경
+                str2+=(char)(c+32);
+            }
+            else if(c>=97&&c<=122){ //소문자를 대문자로 변경
+                str2+=(char)(c-32);
+            }
+        }//for_end
+        System.out.println(str2);
     }
 }
