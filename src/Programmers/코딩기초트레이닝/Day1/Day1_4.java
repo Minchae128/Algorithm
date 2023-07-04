8574a27fff783f9bc436a442f7d42cc7aa030e64
smc <minchae.shin1@gmail.com>
2023-07-04 13:18:04 +0900

    23.07.04 Algorithm

---
+++ src/Programmers/코딩기초트레이닝/Day1/Day1_4.java
@@ -0,0 +1,8 @@
+package src.Programmers.코딩기초트레이닝.Day1;
+
+//문자열 출력하기
+public class Day1_4 {
+    public static void main(String[] args){
+        System.out.println("!@#$%^&*(\\'\"<>?:;");
+    }
+}
