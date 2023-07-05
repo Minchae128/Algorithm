d35748e140d0fb706deb10c23b16c5fd4c966f35
smc <minchae.shin1@gmail.com>
2023-07-05 10:25:35 +0900

    23.07.05 Algorithm

---
+++ src/Programmers/코딩기초트레이닝/Day2/Day2_1.java
@@ -0,0 +1,13 @@
+package src.Programmers.코딩기초트레이닝.Day2;
+
+import java.util.Scanner;
+
+public class Day2_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + " + " + b + " = " + (a + b));
+    }
+}
