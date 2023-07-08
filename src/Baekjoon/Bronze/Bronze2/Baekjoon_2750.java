ad63508d56ba97cf32c6acb2ec57fc1ab2fa4805
smc <minchae.shin1@gmail.com>
2023-07-08 21:30:39 +0900

    23.07.08 Algorithm

---
+++ src/Baekjoon/Bronze/Bronze2/Baekjoon_2750.java
@@ -0,0 +1,22 @@
+package src.Baekjoon.Bronze.Bronze2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Baekjoon_2750 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
