bb2bc88e41c0d9d624a183e9aa695a68b4d3e814
shin <minchae.shin1@gmail.com>
2023-08-02 12:19:51 +0900

    23.08.02 Algorithm

---
+++ src/Baekjoon/Bronze/Bronze5/Baekjoon_5339.java
@@ -0,0 +1,17 @@
+package src.Baekjoon.Bronze.Bronze5;
+
+public class Baekjoon_5339 {
+    public static void main(String[] args) {
+
+        System.out.println("     /~\\");
+        System.out.println("    ( oo|");
+        System.out.println("    _\\=/_");
+        System.out.println("   /  _  \\");
+        System.out.println("  //|/.\\|\\\\");
+        System.out.println(" ||  \\ /  ||");
+        System.out.println("============");
+        System.out.println("|          |");
+        System.out.println("|          |");
+        System.out.println("|          |");
+    }
+}
