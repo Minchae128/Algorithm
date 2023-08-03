4a17d76bd52798d60309378de7dc5a311fb6e1b2
shin <minchae.shin1@gmail.com>
2023-08-03 19:32:32 +0900

    23.08.03 Algorithm

---
+++ src/Baekjoon/Bronze/Bronze3/Baekjoon_11719.java
@@ -0,0 +1,15 @@
+package src.Baekjoon.Bronze.Bronze3;
+
+import java.io.*;
+
+public class Baekjoon_11719 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String a;
+
+        while ((a = in.readLine()) != null){
+            System.out.println(a);
+        }
+
+    }
+}
