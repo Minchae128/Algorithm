d3a28630c18cabf749005aec99f199de76eb4a71
shin <minchae.shin1@gmail.com>
2023-09-06 15:02:20 +0900

    23.09.06 Algorithm

---
+++ src/Baekjoon/Bronze/Bronze3/Baekjoon_1085.java
@@ -0,0 +1,20 @@
+package src.Baekjoon.Bronze.Bronze3;
+
+import java.util.Scanner;
+
+public class Baekjoon_1085 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int w = sc.nextInt();
+        int h = sc.nextInt();
+
+        int x_min = Math.min(x, w - x);
+        int y_min = Math.min(y, h - y);
+
+        System.out.println(Math.min(x_min, y_min));
+
+
+    }
+}
