f272e01ba5b804f86aad8fe594f2bc88b6463dc2
shin <minchae.shin1@gmail.com>
2023-09-18 09:36:55 +0900

    23.09.18 Algorithm

---
+++ src/Baekjoon/Bronze/Bronze5/Baekjoon_10870.java
@@ -0,0 +1,21 @@
+package src.Baekjoon.Bronze.Bronze5;
+
+import java.util.Scanner;
+
+public class Baekjoon_10870 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        System.out.println(f(n));
+    }
+
+    //피보나치 함수
+    static int f(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        return f(n - 1) + f(n - 2);
+    }
+}
