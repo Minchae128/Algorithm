4ddc5bcf6ff419a8d68a6ea4c1a4bacb3b7cec8b
shin <minchae.shin1@gmail.com>
2023-09-20 12:10:13 +0900

    23.09.20 Algorithm

--- src/Baekjoon/Bronze/Bronze2/Baekjoon_5622.java
+++ src/Baekjoon/Bronze/Bronze2/Baekjoon_5622.java
@@ -1,33 +1,75 @@
 package src.Baekjoon.Bronze.Bronze2;
+//다이얼
+/*주의할점
+ * 1. 입력은 대문자로 이루어져있다.
+ * 2. 시간의 합을 구하는 문제*/
 
 import java.util.Scanner;
 
-public class Baekjoon_1152 {
+
+public class Baekjoon_5622 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         String n = sc.nextLine(); //문자열 입력 받기
-        String[] arr = n.split(" "); //공백을 기준으로 나눠서 배열에 저장
-        if (arr.length == 0) { //만약 아무것도 입력하지 않았다면 배열의 크기가 0이므로 배열[0] 이런식으로 접근하면 에러가 뜨므로 아예 0을 프린트 해주고 프로그램을 종료합니다.
-            System.out.println(0);
-            System.exit(0); //프로그램 강제 종료
+
+        int count = 0;
+        int k = n.length();
+
+        for (int i = 0; i < k; i++) {
+            switch (n.charAt(i)) {
+
+                case 'A':
+                case 'B':
+                case 'C':
+                    count += 3;
+                    break;
+
+                case 'D':
+                case 'E':
+                case 'F':
+                    count += 4;
+                    break;
+
+                case 'G':
+                case 'H':
+                case 'I':
+                    count += 5;
+                    break;
+
+                case 'J':
+                case 'K':
+                case 'L':
+                    count += 6;
+                    break;
+
+                case 'M':
+                case 'N':
+                case 'O':
+                    count += 7;
+                    break;
+
+                case 'P':
+                case 'Q':
+                case 'R':
+                case 'S':
+                    count += 8;
+                    break;
+
+                case 'T':
+                case 'U':
+                case 'V':
+                    count += 9;
+                    break;
+
+                case 'W':
+                case 'X':
+                case 'Y':
+                case 'Z':
+                    count += 10;
+                    break;
+            }
         }
-        if (arr[0] == "") { //배열의 맨 앞이 공백이면 공백은 단어가 아니므로 길이 -1
-            System.out.println(arr.length - 1);
-        }  else { //그 외에는 정상적으로 길이 출력
-            System.out.println(arr.length);
-        }
+        System.out.println(count);
     }
 }
-
-/*
-* public class Baekjoon_1152 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String n = sc.nextLine();
-        String[] arr = n.split(" ");
-
-        System.out.println(arr.length);
-    }
-}
-예외를 생각하지 못하고 코딩했다;;*/
