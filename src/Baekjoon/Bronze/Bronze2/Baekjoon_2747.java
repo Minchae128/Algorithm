64d6525ba991b11db3ce1e07050579bc90f4334f
shin <minchae.shin1@gmail.com>
2023-09-21 22:31:33 +0900

    23.09.21 Algorithm

---
+++ src/Baekjoon/Bronze/Bronze2/Baekjoon_2747.java
@@ -0,0 +1,23 @@
+package src.Baekjoon.Bronze.Bronze2;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon_2747 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[46];
+        a[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            a[i] = a[i - 1] + a[i - 2];
+        }
+
+        System.out.println(a[n]);
+    }
+
+}
