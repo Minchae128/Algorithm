42076375cbda2ec1f177048176edadf4c2bc4195
shin <minchae.shin1@gmail.com>
2023-09-24 22:01:57 +0900

    23.09.24 Algorithm

---
+++ src/Programmers/코딩테스트입문/Day1/Day1_4.java
@@ -0,0 +1,10 @@
+package src.Programmers.코딩테스트입문.Day1;
+//두 수의 곱
+
+public class Day1_4 {
+    public int solution(int num1, int num2) {
+        int result = num1 / num2;
+
+        return result;
+    }
+}
