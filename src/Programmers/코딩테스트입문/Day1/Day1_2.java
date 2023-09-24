1dbcdbe0bc46019f0cc8250d0f24454902a953ed
shin <minchae.shin1@gmail.com>
2023-09-24 22:00:08 +0900

    23.09.24 Algorithm

---
+++ src/Programmers/코딩테스트입문/Day1/Day1_2.java
@@ -0,0 +1,10 @@
+package src.Programmers.코딩테스트입문.Day1;
+//두 수의 차
+
+public class Day1_2 {
+    public int solution(int num1, int num2) {
+        int result = num1 - num2;
+
+        return result;
+    }
+}
