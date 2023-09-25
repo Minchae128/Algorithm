975668004bb1f36da5344cac100b04a97d320822
shin <minchae.shin1@gmail.com>
2023-09-25 11:47:11 +0900

    23.09.25 Algorithm

---
+++ src/Programmers/코딩테스트입문/Day2/Day2_2.java
@@ -0,0 +1,15 @@
+package src.Programmers.코딩테스트입문.Day2;
+//숫자 비교하기
+
+public class Day2_2 {
+    public int solution(int num1, int num2) {
+        int answer = 1;
+
+        if (num1 == num2) {
+            return answer;
+        } else {
+            return -1;
+        }
+
+    }
+}
