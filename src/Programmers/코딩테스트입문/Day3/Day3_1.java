2836cca21af7cd9450c87b8f7eeed383cf1ce398
shin <minchae.shin1@gmail.com>
2023-09-27 18:05:25 +0900

    23.09.27 Algorithm

---
+++ src/Programmers/코딩테스트입문/Day3/Day3_1.java
@@ -0,0 +1,11 @@
+package src.Programmers.코딩테스트입문.Day3;
+//나머지 구하기
+
+public class Day3_1 {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+
+        answer = num1 % num2;
+        return answer;
+    }
+}
