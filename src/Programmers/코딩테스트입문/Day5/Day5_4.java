a094925b29291be552d14a8d50dc71b95330b518
shin <minchae.shin1@gmail.com>
2023-10-09 17:27:17 +0900

    23.10.09 Algorithm

---
+++ src/Programmers/코딩테스트입문/Day5/Day5_4.java
@@ -0,0 +1,12 @@
+package src.Programmers.코딩테스트입문.Day5;
+
+public class Day5_4 {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+
+        for (int i = 0; i < num_list.length; i++) {
+            answer[i] = num_list[num_list.length - i - 1];
+        }
+        return answer;
+    }
+}
