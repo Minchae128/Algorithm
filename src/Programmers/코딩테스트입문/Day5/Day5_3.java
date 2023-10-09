0f53a09d3a67b7ff1d3e7b204054fbc4b52acba7
shin <minchae.shin1@gmail.com>
2023-10-09 16:58:41 +0900

    23.10.09 Algorithm

---
+++ src/Programmers/코딩테스트입문/Day5/Day5_3.java
@@ -0,0 +1,11 @@
+package src.Programmers.코딩테스트입문.Day5;
+
+public class Day5_3 {
+    public int solution(int age) {
+        int answer = 0;
+
+        answer = 2023- age;
+
+        return answer;
+    }
+}
