4b2dde6d04b563bcdb16978258734fc55e9ab459
shin <minchae.shin1@gmail.com>
2023-10-10 08:08:49 +0900

    23.10.10 Algorithm

--- src/Programmers/코딩테스트입문/Day6/Day6_1.java
+++ src/Programmers/코딩테스트입문/Day6/Day6_1.java
@@ -1,4 +1,7 @@
 package src.Programmers.코딩테스트입문.Day6;
+//StringBuilder : 내부에 버퍼를 가지고 있어 변경 가능한 문자열을 만들때 사용
+//Stringbuilder reverse : 매서드를 사용하면 문자열을 뒤집을 수 있다.
+
 
 public class Day6_1 {
     public String solution(String my_string) {
