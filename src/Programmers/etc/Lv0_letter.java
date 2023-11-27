856e0dc5bbf4ed4a2b0f53752d615de4325d7518
shin <minchae.shin1@gmail.com>
2023-11-27 15:29:04 +0900

    23-11-27 문자열 길이

---
+++ src/Programmers/etc/Lv0_letter.java
@@ -0,0 +1,14 @@
+package src.Programmers.etc;
+
+public class Lv0_letter {
+    public int solution(String message) {
+        int answer = 0;
+
+        answer = message.length() * 2;
+
+        return answer;
+    }
+}
+
+// 문자의 길이를 구할 때 = lenght()
+// 배열의 길이를 구할 때 = lenght
