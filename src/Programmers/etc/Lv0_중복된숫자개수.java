de60a17c4f46645d5b9773b9994eace03ae15e7c
shin <minchae.shin1@gmail.com>
2024-01-15 23:31:45 +0900

    24.01.15 Algorithm

---
+++ src/Programmers/etc/Lv0_중복된숫자개수.java
@@ -0,0 +1,14 @@
+package src.Programmers.etc;
+
+public class Lv0_중복된숫자개수 {
+    public int solution(int[] array, int n) {
+        int answer = 0;
+
+        for(int i = 0; i < array.length; i++){
+            if(n == array[i]){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
