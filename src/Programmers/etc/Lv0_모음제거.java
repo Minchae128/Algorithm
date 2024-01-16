009eae998bb1b58b7268d3db3572806824f8627b
shin <minchae.shin1@gmail.com>
2024-01-16 23:18:04 +0900

    24.01.16 Algorithm 수정

--- src/Programmers/etc/Lv0_모음제거.java
+++ src/Programmers/etc/Lv0_모음제거.java
@@ -1,25 +1,27 @@
-class Solution {
-    public String solution(String my_string) {
-        // 결과 문자열을 구성하기 위해 StringBuilder 사용
-        StringBuilder answer = new StringBuilder();
+package src.Programmers.etc;
 
-        // 주어진 문자열을 문자 배열로 변환
-        char[] array_word = my_string.toCharArray();
+public class Lv0_모음제거 {
+        public String solution(String my_string) {
+            // 결과 문자열을 구성하기 위해 StringBuilder 사용
+            StringBuilder answer = new StringBuilder();
 
-        // 각 문자에 대해 반복
-        for (int i = 0; i < array_word.length; i++) {
-            // 현재 문자
-            char currentChar = array_word[i];
+            // 주어진 문자열을 문자 배열로 변환
+            char[] array_word = my_string.toCharArray();
 
-            // 모음이 아닌 경우에만 결과에 추가
-            // 'a', 'e', 'i', 'o', 'u' 중 하나가 아닌 경우에만 결과 문자열에 추가
-            if (currentChar != 'a' && currentChar != 'e' && currentChar != 'i' && currentChar != 'o' && currentChar != 'u') {
-                // StringBuilder를 사용하여 문자열을 구성
-                answer.append(currentChar);
+            // 각 문자에 대해 반복
+            for (int i = 0; i < array_word.length; i++) {
+                // 현재 문자
+                char currentChar = array_word[i];
+
+                // 모음이 아닌 경우에만 결과에 추가
+                // 'a', 'e', 'i', 'o', 'u' 중 하나가 아닌 경우에만 결과 문자열에 추가
+                if (currentChar != 'a' && currentChar != 'e' && currentChar != 'i' && currentChar != 'o' && currentChar != 'u') {
+                    // StringBuilder를 사용하여 문자열을 구성
+                    answer.append(currentChar);
+                }
             }
-        }
 
-        // StringBuilder를 String으로 변환하여 최종 결과 반환
-        return answer.toString();
-    }
+            // StringBuilder를 String으로 변환하여 최종 결과 반환
+            return answer.toString();
+        }
 }
