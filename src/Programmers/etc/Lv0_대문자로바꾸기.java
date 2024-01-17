1bae3f259ef6ecf38ac428fa445cd395d64689d9
shin <minchae.shin1@gmail.com>
2024-01-17 23:38:30 +0900

    24.01.17 Algorithm

---
+++ src/Programmers/etc/Lv0_대문자로바꾸기.java
@@ -0,0 +1,11 @@
+package src.Programmers.etc;
+
+public class Lv0_대문자로바꾸기 {
+    public String solution(String myString) {
+        String answer = "";
+
+        answer = myString.toUpperCase();
+
+        return answer;
+    }
+}
