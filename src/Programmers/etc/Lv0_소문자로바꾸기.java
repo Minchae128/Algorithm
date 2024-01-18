3447dcc392a840e38ac6d0d645440b6cdc6f6e33
shin <minchae.shin1@gmail.com>
2024-01-18 22:10:56 +0900

    24.01.18 Algorithm

---
+++ src/Programmers/etc/Lv0_소문자로바꾸기.java
@@ -0,0 +1,11 @@
+package src.Programmers.etc;
+
+public class Lv0_소문자로바꾸기 {
+    public String solution(String myString) {
+        String answer = "";
+
+        answer = myString.toLowerCase();
+
+        return answer;
+    }
+}
