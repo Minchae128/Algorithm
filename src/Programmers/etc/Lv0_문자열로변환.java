a60e7e86f7d425c7025855ba947d72a1a8c4628b
shin <minchae.shin1@gmail.com>
2024-01-25 14:34:35 +0900

    24.01.25 Algorithm

---
+++ src/Programmers/etc/Lv0_문자열로변환.java
@@ -0,0 +1,11 @@
+package src.Programmers.etc;
+
+public class Lv0_문자열로변환 {
+    public String solution(int n) {
+        String answer = "";
+
+        answer = Integer.toString(n);
+
+        return answer;
+    }
+}
