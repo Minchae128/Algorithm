03bac10d0501302b1d57e2f350e36b2b3a9a27fa
shin <minchae.shin1@gmail.com>
2024-02-10 23:51:21 +0900

    24.02.10 Algorithm

---
+++ src/Programmers/etc/Lv0_n의배수고르기.java
@@ -0,0 +1,24 @@
+package src.Programmers.etc;
+
+public class Lv0_n의배수고르기 {
+    public int[] solution(int n, int[] numlist) {
+        int j = 0;
+        int count = 0;
+
+        for (int i = 0; i < numlist.length; i++) {
+            if (numlist[i] % n == 0) {
+                count++;
+            }
+        }
+
+        int[] answer = new int[count];
+
+        for (int i = 0; i < numlist.length; i++) {
+            if (numlist[i] % n == 0) {
+                answer[j] = numlist[i];
+                j++;
+            }
+        }
+        return answer;
+    }
+}
